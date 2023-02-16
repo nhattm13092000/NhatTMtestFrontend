520acf133a0a0a8e5f89aff7488fc484ae14981a
nhattm13092000 <trinhnhathhh@gmail.com>
2023-02-17 01:27:22 +0700

    Code test Frontend

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('.slideup').each(function(){
+            var x = $(this).offset().top;
+
+            var y = $(window).scrollTop();
+            if(x < y + 550){
+                $(this).addClass('slide');
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#menutab").click(function() {
+        $(".top__center").toggle();
+    });
+});
